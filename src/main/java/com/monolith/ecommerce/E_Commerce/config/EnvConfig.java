aa516c733f6db364fe01a98643c9ea7334d9c8c9
RajChauhan21 <craj47577@gmail.com>
2025-11-07 15:45:58 +0530

    fixed env file

--- src/main/java/com/monolith/ecommerce/E_Commerce/config/EnvConfig.java
+++ src/main/java/com/monolith/ecommerce/E_Commerce/config/EnvConfig.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.io.support.PropertySourceFactory;
 
 @Configuration
-@PropertySource(factory = PropertySourceFactory.class, value = "classpath:.env")
+@PropertySource(factory = PropertySourceFactory.class, value = "classpath:.env", ignoreResourceNotFound = true)
 public class EnvConfig {
     // No static block needed
 }
